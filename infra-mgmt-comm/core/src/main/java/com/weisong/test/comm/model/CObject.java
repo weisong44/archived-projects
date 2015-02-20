78d0795239cd542759951336974b296d347f69c6
Wei Song <weisong44@hotmail.com>
2015-02-19 21:06:56 -0800

    Added back 4 projects

---
+++ infra-mgmt-comm/core/src/main/java/com/weisong/test/comm/model/CObject.java
@@ -0,0 +1,12 @@
+package com.weisong.test.comm.model;
+
+import java.io.Serializable;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+abstract public class CObject implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ infra-mgmt-parent-pom/README.md
@@ -0,0 +1,2 @@
+infra-mgmt-parent-pom
+=====================
